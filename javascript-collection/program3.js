24f240e66d9baf448b5f9c05c076ab2700fa3182
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-05 16:24:41 +0800

    Create program3.js

---
+++ javascript-collection/program3.js
@@ -0,0 +1,18 @@
+// p3 data types
+
+// primitive
+let studentName = "Ferringson";
+let studentAge = 25;
+let isPlayerAlive = true;
+
+let bigNumber1 = 89751234654315649346n; // there is a letter n at the end
+let bigNumber2 = BigInt(89751234654315649346n);
+
+let message;
+let value1 = null;
+const value2 = Symbol();
+
+// object
+let playerObject = {
+  health: 73924
+};
