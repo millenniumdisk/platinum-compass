6a7e01ba0ef428c14737fb8e29a159ab9534777d
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-05 16:57:01 +0800

    Create program6.js

---
+++ javascript-collection/program6.js
@@ -0,0 +1,23 @@
+// p6 operators
+
+console.log(7 + 9); // addition operator
+console.log(987 - 110); // subtraction operator
+console.log(10 / 3); // division operator
+
+let number1 = 10;
+number1 = number1 + 1;
+
+let number2 = 10;
+number2++; // increment operator
+
+console.log(number1);
+console.log(number2);
+
+let number3 = 7;
+number3 = number3 + 9;
+
+let number4 = 7
+number4 += 9;
+
+console.log(number3);
+console.log(number4);
