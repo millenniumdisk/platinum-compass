a8aadba00a9b6371f43fd326f755dc43d8334214
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-05 16:40:33 +0800

    Create program4.js

---
+++ javascript-collection/program4.js
@@ -0,0 +1,19 @@
+// p4 declaring variables with let and changing their values
+"use strict";
+
+let value1;
+value1 = 73;
+
+let value2 = 850;
+value2 = "The weather today is nice";
+
+let a, b, c;
+a = 778;
+b = 96;
+c = -741236;
+let x = 82, y = 26, z = -4;
+
+console.log(value1)
+console.log(value2);
+console.log(a, b, c);
+console.log(x, y, z);
