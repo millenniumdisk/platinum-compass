f51596c3a23f25fa334adebb431ca2002e3ea2f0
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-05 17:04:03 +0800

    Update p6 operators.js

--- javascript-collection/p6 operators.js
+++ javascript-collection/p6 operators.js
@@ -3,8 +3,9 @@
 console.log(7 + 9); // addition operator
 console.log(987 - 110); // subtraction operator
 console.log(10 / 3); // division operator
+console.log(3 ** 3); // exponentiation operator
 
-let number1 = 10;
+let number1 = 10; // assignment operator
 number1 = number1 + 1;
 
 let number2 = 10;
@@ -21,3 +22,17 @@ number4 += 9;
 
 console.log(number3);
 console.log(number4);
+
+let number5 = 10;
+
+number5--;
+console.log(number5);
+
+number5 -= 3;
+console.log(number5);
+
+number5 *= 2;
+console.log(number5);
+
+number5 /= 3;
+console.log(number5);
