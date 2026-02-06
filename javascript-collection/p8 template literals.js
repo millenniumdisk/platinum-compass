37f48102a7219aee7e9803cc270b6ad19296360f
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-06 10:57:51 +0800

    Update p8 template literals.js

--- javascript-collection/p8 template literals.js
+++ javascript-collection/p8 template literals.js
@@ -1,1 +1,7 @@
+// p8 template literals
+"use strict";
 
+const numOfPlayers = 56;
+
+// use backticks or ` to enclose a string with variables inside to have a string as a result
+console.log(`The number of players in this game for now is ${numOfPlayers} and it is still growing`);
