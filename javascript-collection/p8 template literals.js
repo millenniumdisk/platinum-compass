dd1511a4c4f73df6197ba6b0cd403aea87a386a5
millenniumdisk <millenniumdiskette@gmail.com>
2026-02-06 11:01:38 +0800

    Update p8 template literals.js

--- javascript-collection/p8 template literals.js
+++ javascript-collection/p8 template literals.js
@@ -2,6 +2,18 @@
 "use strict";
 
 const numOfPlayers = 56;
+const message1 = "This is a multi line but there is a\n\
+  need to use a newline in order to break the long string\n\
+  into multiple lines"
+
+const message2 = `This is a multi line string which is
+  long but is also in multiple
+  lines because it is easier to
+  see the contents instead of one
+  long line with everything in it`;
 
 // use backticks or ` to enclose a string with variables inside to have a string as a result
 console.log(`The number of players in this game for now is ${numOfPlayers} and it is still growing`);
+
+console.log(message1);
+console.log(message2);
